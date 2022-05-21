1f03088ceea6ad8ad04063ad4b138d7cafaf7c1e
Jiri Novotny <novotny.049@gmail.com>
2022-05-21 15:02:47 +0300

    operations implemented

--- lib/src/exptree.rs
+++ lib/src/exptree.rs
@@ -1,29 +1,115 @@
-use std::ops::{Not, BitAnd, BitOr, BitXor};
-use std::cmp::{PartialEq, Eq};
-pub struct BinData<T> {
-    data: T,
-    left: BinTree<T>,
-    right: BinTree<T>,
-}
-pub struct BinTree<T>(Option<Box<BinData<T>>>);
-
-impl<T> BinTree<T> {
-    pub fn new() -> Self {
-        BinTree(None)
-    }
-}
+use std::cmp::{Eq, PartialEq};
+use std::ops::{BitAnd, BitOr, BitXor, Not};
 
 pub enum ExpNode<T> {
     Leaf(T),
-    Branch {
+    TwoOp {
         left: Box<ExpNode<T>>,
         right: Box<ExpNode<T>>,
         op: Box<dyn Fn(T, T) -> T>,
     },
+    OneOp {
+        left: Box<ExpNode<T>>,
+        op: Box<dyn Fn(T) -> T>,
+    },
 }
 
 impl<T> ExpNode<T>
-where T: Not, BitAnd<Output = T> + BitOr<Output = T> + BitXor<Output = T> + PartialEq;
+where
+    T: Not<Output = T>,
 {
+    pub fn negation<L>(left: L) -> Self
+    where
+        L: Into<ExpNode<T>>,
+    {
+        Self::OneOp {
+            left: Box::new(left.into()),
+            op: Box::new(|l| !l),
+        }
+    }
+}
 
+impl<T> ExpNode<T>
+where
+    T: BitAnd<Output = T>,
+{
+    pub fn and<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<ExpNode<T>>,
+        R: Into<ExpNode<T>>,
+    {
+        Self::TwoOp {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| l & r),
+        }
+    }
+}
+
+impl<T> ExpNode<T>
+where
+    T: BitOr<Output = T>,
+{
+    pub fn or<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<ExpNode<T>>,
+        R: Into<ExpNode<T>>,
+    {
+        Self::TwoOp {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| l | r),
+        }
+    }
+}
+
+impl<T> ExpNode<T>
+where
+    T: BitXor<Output = T>,
+{
+    pub fn xor<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<ExpNode<T>>,
+        R: Into<ExpNode<T>>,
+    {
+        Self::TwoOp {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| l ^ r),
+        }
+    }
+}
+
+impl<T> ExpNode<T>
+where
+    T: Not<Output = T> + BitAnd<Output = T>,
+{
+    pub fn mat_condition<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<ExpNode<T>>,
+        R: Into<ExpNode<T>>,
+    {
+        Self::TwoOp {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| !(l & !r)),
+        }
+    }
+}
+
+impl<T> ExpNode<T>
+where
+    T: PartialEq + Eq + From<bool>,
+{
+    pub fn eq<L, R>(left: L, right: R) -> Self
+    where
+        L: Into<ExpNode<T>>,
+        R: Into<ExpNode<T>>,
+    {
+        Self::TwoOp {
+            left: Box::new(left.into()),
+            right: Box::new(right.into()),
+            op: Box::new(|l, r| (l == r).into()),
+        }
+    }
 }
