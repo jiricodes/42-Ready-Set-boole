fc43bed94e34b3f1f7cef0ad8f47683a894b5ccc
Jiri Novotny <novotny.049@gmail.com>
2024-11-02 07:45:00 +0200

    resources

--- resources/resources.md
+++ resources/resources.md
@@ -4,6 +4,12 @@ List of useful resources.
 ## [Subject](ready_set_boole.en.pdf)
 The project instructions as given by the 42 curriculum.
 
+## Russian peasant method for multiplication
+- [Wiki](https://en.wikipedia.org/wiki/Ancient_Egyptian_multiplication#Russian_peasant_multiplication)
+- [Geeks for Geeks](https://www.geeksforgeeks.org/russian-peasant-multiply-two-numbers-using-bitwise-operators/)
+- [Nice explanation](https://www.themathdoctors.org/russian-peasant-multiplication-how-and-why/)
+
 ## Various
 - [Polish notation and binary trees](https://sites.math.northwestern.edu/~mlerma/courses/cs310-04w/notes/dm-bintrees.pdf)
 - [BinTree blog post](https://medium.com/swlh/rust-binary-tree-a-refactor-1b090a88e24)
+
--- rsb-lib/src/adder.rs
+++ rsb-lib/src/adder.rs
@@ -1,19 +1,3 @@
-
-//! Adder
-//! You must write a function that takes as parameters two natural numbers
-//! a and b and returns one natural number that equals a + b.
-//! However the only operations you’re allowed to use are:
-//!     - & (bitwise AND)
-//!     - | (bitwise OR)
-//!     - ^ (bitwise XOR)
-//!     - • << (left shift)
-//!     - • >> (right shift)
-//!     - • = (assignment)
-//!     - • ==, !=, <, >, <=, >= (comparison operators)
-//!
-//! The incrementation operator (++ or += 1) is allowed only to increment
-//! the index of a loop and must not be used to compute the result itself.
-
 /// This is the simplest solution to an adder function.
 /// Since we are working with 32 bits, there's no stack overflow danger.
 /// O(32) -> O(1)
@@ -86,13 +70,5 @@ mod tests {
         a = 1073741823;
         b = 2147483647;
         assert_eq!(a + b, adder(a, b), "{} + {}", a, b);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_adder_panic() {
-        let a = u32::MAX;
-        let b = 5;
-        adder(a, b);
     }
 }
